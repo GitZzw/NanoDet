b8b01906b4dd3d1a29e269a6c99cda38e271df79
GitZzw <120137016@qq.com>
2021-01-12 21:35:43 +0800

    all

--- tools/export.py
+++ tools/export.py
@@ -13,8 +13,8 @@ def main(config, model_path, output_path, input_shape=(320, 320)):
     print('finished exporting onnx ')
 
 if __name__ == '__main__':
-    cfg_path = r"config/test.yml"
-    model_path = r"model/model_last.pth"
-    out_path = r'onnx/output.onnx'
+    cfg_path = r"config/nanodet-m.yml"
+    model_path = r"model/nanodet_m.pth"
+    out_path = r'onnx/output2.onnx'
     load_config(cfg, cfg_path)
     main(cfg, model_path, out_path, input_shape=(320, 320))
